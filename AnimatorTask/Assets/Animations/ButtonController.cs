cb40e92c68c4abfa49b290e7be626354e348ee45
timurg <timurg@playtika.com>
2020-10-27 20:38:09 +0300

    Buttom

---
+++ AnimatorTask/Assets/Animations/ButtonController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    private Animator _animator = default;
+
+    private int _jump = 0;
+    private int _rotate = 0;
+    private int _scale = 0;
+    private int _color = 0;
+    private int _clickTrigger = -1;
+    
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _jump = Animator.StringToHash("Jump");
+        _rotate = Animator.StringToHash("Rotation");
+        _scale = Animator.StringToHash("Scale");
+        _color = Animator.StringToHash("Color");
+        _clickTrigger = Animator.StringToHash("Click");
+    }
+
+    public void OnClick()
+    {
+        _animator.SetFloat(_color, Random.Range(0f, 1f));
+        _animator.SetFloat(_rotate, Random.Range(0f, 1f));
+        _animator.SetFloat(_scale, Random.Range(0f, 1f));
+        _animator.SetFloat(_jump, Random.Range(0f, 1f));
+        _animator.SetTrigger(_clickTrigger);
+    }
+}
---
+++ AnimatorTask/Assets/CharacterController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CharacterController : MonoBehaviour
+{
+    private Animator _animator = default;
+
+    private int _jumpTrigger = -1;
+    private int _walkSpeed = -1;
+    private int _crouch = -1;
+
+    private Transform _transform;
+
+    private Vector3 _leftScale;
+    private Vector3 _rightScale;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _crouch = Animator.StringToHash("IsCrouch");
+        _walkSpeed = Animator.StringToHash("WalkSpeed");
+        _jumpTrigger = Animator.StringToHash("Jump");
+
+        _transform = transform;
+        
+        var localScale = _transform.localScale;
+        _leftScale = localScale;
+        _leftScale.x = -_leftScale.x;
+        
+        _rightScale = localScale;
+    }
+
+    
+    // A/D - movement, Space - jump, C - crouch
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger(_jumpTrigger);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _animator.SetInteger(_walkSpeed, 1);
+            _transform.localScale = _leftScale;         // Turn character in walk direction
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _animator.SetInteger(_walkSpeed, 1);
+            _transform.localScale = _rightScale;         // Turn character in walk direction
+        }
+        else
+        {
+            _animator.SetInteger(_walkSpeed, 0);
+        }
+
+        _animator.SetBool(_crouch, Input.GetKey(KeyCode.C));
+    }
+}
